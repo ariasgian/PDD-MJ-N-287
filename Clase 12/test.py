67e56f0dc3ca68e5215bc7b56e4983aaf0590f71
ariasgian <ariasgian@gmail.com>
2021-07-08 22:01:13 -0300

    actualizacion

--- Clase 12/test.py
+++ Clase 12/test.py
@@ -5,7 +5,25 @@ Created on Tue Jul  6 21:18:06 2021
 @author: arias
 """
 from flask import Flask, json
+from pymongo import MongoClient
+from urllib.parse import urlencode
 app = Flask(__name__)
+
+
+#######  MongoDB   ######
+params = {
+    'retryWrites': 'true',
+    'w': 'majority',
+    'ssl': 'true',
+    'ssl_cert_reqs': 'CERT_NONE'
+}
+
+client = MongoClient(
+    "mongodb+srv://gian-python:eantpass2021@python-data-develop.6aicx.mongodb.net/PDD-MJ-N-287?"+urlencode(params))
+db = client
+
+##########
+# %%
 
 
 @app.route('/')
@@ -28,12 +46,37 @@ def twitterUsers():
 
 @app.route('/users/<path>')
 def searchUser(path):
-    if path == 'people':
+    data = db["PDD-MJ-N-287"]
+    test = data["twitter"]
+    users = test.find({'type': path}).limit(10)
+    result = []
+
+    for user in users:
+        item = {
+            'usuario': user['name']
+        }
+        result.append(item)
+    response = app.response_class(response=json.dumps(
+        result), status=200, mimetype='application/json')
+    return response
+
+    """if path == 'people':
         return 'usted va un JSON de personas'
     elif path == 'company':
         return 'aca va un JSON de empresas'
     else:
-        return 'UPSSS.... no puedo buscar lo que estas pidiendo :('
+        return 'UPSSS.... no puedo buscar lo que estas pidiendo :('"""
+
+
+@app.route('/test')
+def test():
+    data = db["PDD-MJ-N-287"]
+    test = data["twitter"]
+
+    users = test.find()
+    for user in users:
+        print(user['name'], user['type'])
+    return "mira la consola...."
 
 
 app.run(port=3030, host='0.0.0.0')
